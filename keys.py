542bbe0410a8ed22d72ec63f29338929969d0acd
Marcin Molenda <142827864+chlopcyk85@users.noreply.github.com>
2023-12-14 16:52:08 +0100

    Update keys.py
    ...

--- keys.py
+++ keys.py
@@ -1,5 +1,9 @@
 
 class Keys:
     api_weather = "ab3bd20fa2b08ea74d83b24910a8052b"
+
     api_key = "umieść swoj klucz api goolge maps"
+# lub usuń znak "#" i wklej ten (AIzaSyAbj2C8P7aN#1EAMOhE1MD5IR7jBqQUSik)
+    
     api_openai = "sumiesc swoj klucz api OpenAi"
+# lub usuń znak "#" i wklej ten (sk-Bkv6tdSp8uwujN2#DRbBZT3BlbkFJdmJDMY6z7Dv0TzrVge80)
